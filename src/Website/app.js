eff0dc0f088386d5e5aacde8115d1268c32bf670
Unknown <daniel.flataker@gmail.com>
2018-03-30 23:28:24 +0200

    Adding features

--- src/Website/app.js
+++ src/Website/app.js
@@ -1,20 +1,34 @@
 // Modules
 const express = require(`express`);
+const favicon = require('serve-favicon')
+const path = require(`path`);
 const bodyParser = require(`body-parser`);
 const mongoose = require(`mongoose`);
-const path = require(`path`);
 
 // Constants
 const port = 3000;
 const app = express();
 
-// Middleware
+// Middleware - View Engine
+app.set(`view engine`, 'ejs');
+app.set(`views`, path.join(__dirname, `views`));
+
+// Middleware - BodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// Middleware - Set Static Path
 app.use(express.static(path.join(__dirname, `public`)));
+
+// Middelware - Set Favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+
+// Middleware - Discord.js
+app.use('/api/discord', require('./api/discord'));
 
 // Routes
 app.get(`/`, (req, res) => {
+  res.render(`index`);
 });
 
 // Listen to port
