3d35c9df58b7abb42505cbf91771e351543a84f5
Unknown <daniel.flataker@gmail.com>
2018-03-30 21:55:00 +0200

    Created command action list

---
+++ src/Bot/cmds/actionlist.js
@@ -0,0 +1,28 @@
+// Import Model
+const Action = require('../models/action');
+const error = require('../util/error');
+const Discord = require('discord.js');
+
+exports.run = a => {
+
+  // Find the actions
+  Action.find({guild: a.message.guild.id}).exec((err, actions) => {
+
+    // If error, return
+    if (err) return a.message.channel.send(`And error occured`);
+
+    // If no action, return
+    if (!actions[0]) {
+      return a.message.channel.send(`You haven't created any actions yet`);
+    }
+
+    // Send action info
+    const embed = new Discord.RichEmbed()
+      .setColor(3447003)
+      .setTitle(`__**${a.message.guild.name}'s Custom Actions**__`);
+    actions.forEach(act => {
+      embed.addField(`${act.name}`, `type: ${act.actionType}\nvalue: ${act.value}`);
+    });
+    a.message.channel.send({embed});
+  })
+}
