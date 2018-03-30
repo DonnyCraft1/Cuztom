4aa6be49947bdf40d45c4ed8ee5295f3dd820e68
Unknown <daniel.flataker@gmail.com>
2018-03-30 21:54:02 +0200

    Created help command

---
+++ src/Bot/cmds/help.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+exports.run = a => {
+
+  const embed = new Discord.RichEmbed()
+    .setColor(3447003)
+    .setTitle(`Cuztom - The Custom Command Bot`)
+
+    .setDescription(`
+      Hi! I'm ***Cuztom***.
+      Unlike other bots, I have no limitations for custom commands.
+      This bot is realy powerfull and customizable!
+      We also have a really helpfull support guild, and it's really easy to get started.
+    `)
+
+    .addField(`Contact`, `
+      Join my **[guild](https://discord.gg/Jyj3esH)** for support!
+      Check out the **[wiki (under dev)](https://github.com/DonnyCraft1/Cuztom/wiki)** to get started!
+      The online **[dashboard (under dev)](#)** makes it really easy to set up custom commands!
+    `)
+
+    .addField(`Get Started`, `
+      \`${a.config.prefix}cmdcreate <name>\` creates a new command
+      \`${a.config.prefix}actioncreate <name>\` creates a new action
+      \`${a.config.prefix}cmdaddaction <command> <action>\` adds an action to a command
+
+      \`${a.config.customCmdPrefix}\` is the prefix for custom commands
+      \`${a.config.customCmdPrefix}<commandname>\` runs the custom command
+
+      Good luck! For more detailed information, please visit the **[support guild](https://discord.gg/Jyj3esH)**, or the **[wiki (under dev)](https://github.com/DonnyCraft1/Cuztom/wiki)**!
+    `);
+
+  a.message.channel.send({embed});
+}
