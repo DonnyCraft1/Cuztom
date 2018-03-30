8de55d83fa6553dcff16d862dd9acf34567a8d18
Unknown <daniel.flataker@gmail.com>
2018-03-30 21:56:12 +0200

    Now it says listening to, instead of playing

--- src/Bot/events/ready.js
+++ src/Bot/events/ready.js
@@ -3,7 +3,10 @@ const config = require('../config.json');
 module.exports = client => {
   console.log(client.user.username + ' is online');
   client.user.setPresence({
-    game: {name: `${config.customCmdPrefix}help`},
+    game: {
+      name: `${config.prefix}help`,
+      type: 2
+    },
     status: `dnd`
   });
 }
