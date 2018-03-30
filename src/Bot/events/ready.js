0c4ed9a4f091d82176716234d3b487aff5992398
Unknown <daniel.flataker@gmail.com>
2018-03-30 16:39:38 +0200

    Added playing message

--- src/Bot/events/ready.js
+++ src/Bot/events/ready.js
@@ -1,4 +1,9 @@
 const config = require('../config.json');
+
 module.exports = client => {
   console.log(client.user.username + ' is online');
+  client.user.setPresence({
+    game: {name: `${config.customCmdPrefix}help`},
+    status: `dnd`
+  });
 }
