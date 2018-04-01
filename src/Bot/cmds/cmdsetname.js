a8e6ec2a963d8730c5f8144c77aa7552c3999849
Unknown <daniel.flataker@gmail.com>
2018-04-01 16:11:49 +0200

    Fixed bug
    Fixed bug where you could rename an command to create duplicate command names.

--- src/Bot/cmds/cmdsetname.js
+++ src/Bot/cmds/cmdsetname.js
@@ -8,9 +8,13 @@ exports.run = a => {
   if (!a.args[1]) return a.message.channel.send(`Please provide a new name for the command`);
 
   // Find the command
-  Command.find({name: a.args[0], guild: a.message.guild.id}).exec((err, command) => {
+  Command.find({name: a.args[0], guild: a.message.guild.id}).exec( async (err, command) => {
     if (!command[0]) return a.message.channel.send(`I did not find the command`);
-    if (command[1]) return a.message.channel.send(error.message(1002));
+    if (command[1]) return a.message.channel.send(error.message(1001));
+    
+    const newCommand = await Command.find({name: a.args[1], guild: a.message.guild.id}).exec();
+    if (newCommand[1]) return a.message.channel.send(error.message(1001));
+    if (newCommand[0]) return a.message.channel.send(`There is already a command with the name **${newCommand[0].name}**. The name was not changed`);
 
     command[0].update({name: a.args[1]}, (err, result) => {
       if (err || !result.n > 0) return a.message.channel.send(`An error occured, couldn't update command name`);
