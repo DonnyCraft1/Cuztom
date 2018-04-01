be6f9795498537ae72ea1caca6a3e86038c237d0
Unknown <daniel.flataker@gmail.com>
2018-04-01 16:11:30 +0200

    Fixed bug
    Fixed bug where you could rename an action to create duplicate action names.

--- src/Bot/cmds/actionsetname.js
+++ src/Bot/cmds/actionsetname.js
@@ -8,9 +8,13 @@ exports.run = a => {
   if (!a.args[1]) return a.message.channel.send(`Please provide a new name for the action`);
 
   // Find the action
-  Action.find({name: a.args[0], guild: a.message.guild.id}).exec((err, action) => {
+  Action.find({name: a.args[0], guild: a.message.guild.id}).exec( async (err, action) => {
     if (!action[0]) return a.message.channel.send(`I did not find the action`);
     if (action[1]) return a.message.channel.send(error.message(1002));
+    
+    const newAction = await Action.find({name: a.args[1], guild: a.message.guild.id}).exec();
+    if (newAction[1]) return a.message.channel.send(error.message(1002));
+    if (newAction[0]) return a.message.channel.send(`There is already an action with the name **${newAction[0].name}**. The name was not changed`);
 
     action[0].update({name: a.args[1]}, (err, result) => {
       if (err || !result.n > 0) return a.message.channel.send(`An error occured, couldn't update action name`);
